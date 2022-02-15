ff7bc59b84233236c4f0ed320b095681f65e1997
Bhavesh233 <bhaveshk@prominentpixel.com>
2022-02-15 19:13:24 +0530

    create Project-MVC

---
+++ Project-MVC/src/main/java/com/luv2code/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,16 @@
+package com.luv2code.springdemo.dao;
+
+import java.util.List;
+
+import com.luv2code.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	void saveCustomer(Customer theCustomer);
+
+	Customer getCustomer(int theId);
+
+	void deleteCustomer(int theId);
+}
