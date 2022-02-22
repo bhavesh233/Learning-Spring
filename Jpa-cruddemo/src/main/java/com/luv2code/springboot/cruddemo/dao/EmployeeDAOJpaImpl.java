1fe97c4dff6ad4e8930394e9e9aba0a3da5dc790
Bhavesh233 <bhaveshk@prominentpixel.com>
2022-02-22 19:48:48 +0530

    create spring-boot jpa

--- Jpa-cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ Jpa-cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
